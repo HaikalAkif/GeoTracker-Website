19191d8fa970f8840bdb5605f862b37c77b48ec6
HaikalAkif <haikalakif17@gmail.com>
2023-01-07 01:13:18 +0800

    New Popup Feature

---
+++ popup.js
@@ -0,0 +1,19 @@
+let popupS = document.getElementById("popupS");
+
+    function openPopupS(){
+        popupS.classList.add("open-popupS");
+    }
+    
+    function closePopupS(){
+        popupS.classList.remove("open-popupS");
+    }
+
+let popupL = document.getElementById("popupL");
+
+    function openPopupL(){
+        popupL.classList.add("open-popupL");
+    }
+    
+    function closePopupL(){
+        popupL.classList.remove("open-popupL");
+    }
