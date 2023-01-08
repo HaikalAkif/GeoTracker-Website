0d67175108853bd4a6c7c40c8cad56d6ee83afb7
HaikalAkif <haikalakif17@gmail.com>
2023-01-09 00:07:56 +0800

    Files reposition

---
+++ JavaScript/gsap.js
@@ -0,0 +1,198 @@
+// Home Page Gsap
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".search-title", {
+    scrollTrigger: ".search-title",
+    y: 100,
+    duration: 1.2,
+    opacity: 0,
+    scrub: 1,
+});
+
+gsap.from(".search", {
+    scrollTrigger: ".search",
+    y: 100,
+    duration: 1.2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont2-title", {
+    scrollTrigger: ".cont2-title",
+    x: -100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".pic", {
+    scrollTrigger: ".left",
+    x: -100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont3-title", {
+    scrollTrigger: ".cont3-title",
+    x: 100,
+    duration: 2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont3-st", {
+    scrollTrigger: ".cont3-st",
+    x: 100,
+    duration: 2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont3-title2", {
+    scrollTrigger: ".cont3-title2",
+    x: -100,
+    duration: 2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".feed-left", {
+    scrollTrigger: ".feed",
+    x: -100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".feed-right", {
+    scrollTrigger: ".feed",
+    x: 100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".btn2", {
+    scrollTrigger: ".btn2",
+    x: 100,
+    duration: 1,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".info", {
+    scrollTrigger: ".cont4-top",
+    y: 100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont4-desc", {
+    scrollTrigger: ".cont4-desc",
+    x: -100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".heading", {
+    scrollTrigger: ".heading",
+    x: -100,
+    duration: 1.2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".place", {
+    scrollTrigger: ".small-spots",
+    x: -200,
+    duration: 2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont6-right", {
+    scrollTrigger: ".cont6",
+    x: 100,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".promote", {
+    scrollTrigger: ".cont7-top",
+    y: 150,
+    duration: 1.2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".btn5", {
+    scrollTrigger: ".btn5",
+    y: 150,
+    duration: 1.2,
+    opacity: 0,
+    scrub: 1
+});
+
+// Help Page Gsap
+
+gsap.from(".choice", {
+    scrollTrigger: ".container2",
+    y: 100,
+    duration: 2,
+    opacity: 0,
+    scrub: 2
+});
+
+gsap.from(".btnD", {
+    scrollTrigger: ".btn",
+    y: 150,
+    duration: 1,
+    opacity: 0,
+    scrub: 1
+});
+
+// About Page Gsap
+
+gsap.from(".head-title", {
+    scrollTrigger: ".title",
+    y: 200,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".staff", {
+    scrollTrigger: ".staff",
+    y: 200,
+    duration: 0.8,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".sub-title", {
+    scrollTrigger: ".title",
+    y: 150,
+    duration: 2,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".cont3-right", {
+    scrollTrigger: ".container3",
+    x: 150,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+gsap.from(".desc", {
+    scrollTrigger: ".container4",
+    y: 200,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
