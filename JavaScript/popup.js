e6df053777fe855d746ae7ea40f5ce2cc8cee78d
ArifPeycal <ariffaisal021123@gmail.com>
2023-01-10 13:12:33 +0800

    Javascript Popup Updated

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -1,19 +1,37 @@
+let sign_onscreen = false;
+let log_onscreen = false;
+
 let popupS = document.getElementById("popupS");
 
     function openPopupS(){
-        popupS.classList.add("open-popupS");
+        if (sign_onscreen == false && log_onscreen == false) {
+            popupS.classList.add("open-popupS");
+            sign_onscreen = true;
+        }
     }
     
     function closePopupS(){
-        popupS.classList.remove("open-popupS");
+        if (sign_onscreen == true) {
+            popupS.classList.remove("open-popupS");
+            sign_onscreen = false;
+        }
+        
     }
 
 let popupL = document.getElementById("popupL");
 
     function openPopupL(){
-        popupL.classList.add("open-popupL");
+        if (log_onscreen == false && sign_onscreen == false) {
+            popupL.classList.add("open-popupL");
+            log_onscreen = true;
+        }
+        
     }
     
     function closePopupL(){
-        popupL.classList.remove("open-popupL");
+        if (log_onscreen == true) {
+            popupL.classList.remove("open-popupL");
+            log_onscreen = false;
+        }
+        
     }
